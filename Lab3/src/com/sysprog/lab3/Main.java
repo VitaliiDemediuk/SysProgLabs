2054a36db17993917eedde4cdab1dbed9a2af6bb
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2021-10-23 14:16:19 +0300

    start lab3.

---
+++ Lab3/src/com/sysprog/lab3/Main.java
@@ -0,0 +1,38 @@
+package com.sysprog.lab3;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Path getFilePath(String[] args) throws IllegalArgumentException {
+        String arg;
+        if (args.length > 1) {
+            throw new IllegalArgumentException("");
+        }
+
+        if (args.length == 1) {
+            arg = args[0];
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            arg = scanner.nextLine();
+        }
+
+        return FileSystems.getDefault().getPath(arg).toAbsolutePath();
+    }
+
+    public static void main(String[] args)
+    {
+        final Path filePath;
+        try {
+            filePath = getFilePath(args);
+        } catch (IllegalArgumentException ex) {
+            System.err.println(ex.getLocalizedMessage());
+            System.exit(1);
+            return;
+        }
+
+
+    }
+}
